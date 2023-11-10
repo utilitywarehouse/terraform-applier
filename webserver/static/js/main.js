5a6a31403232e43697d2a7cd2a0d6cd4145b1422
Ashok Siyani <ashok.siyani@gmail.com>
2023-11-10 13:25:19 +0000

    re-done GUI for better visibility and access

--- webserver/static/js/main.js
+++ webserver/static/js/main.js
@@ -1,4 +1,9 @@
 function init() {
+  filterModules(window.location.hash)
+  window.onhashchange = function () {
+    filterModules(window.location.hash)
+  }
+
   document.querySelectorAll(".force-module-button").forEach(function (button) {
     button.addEventListener("click", function () {
       // Disable the buttons and close existing alert
@@ -8,15 +13,58 @@ function init() {
         document.querySelector("#force-alert").alert("close")
       }
 
-      forceRun(button.dataset.namespace, button.dataset.name)
+      forceRun(button.dataset.namespace, button.dataset.name, button.dataset.planOnly)
     })
   })
 }
 
+function filterModules(hash) {
+  hash = hash.replace("#", "")
+
+  const namespaceList = document.getElementById("namespace-list");
+  const moduleList = document.getElementById("module-list");
+  const moduleInfoList = document.getElementById("module-info-list");
+
+  for (const child of namespaceList.children) {
+    child.classList.remove("active");
+    // hash can be namespaced name of the module in that case
+    // filter all modules of that namespace
+    if (hash.startsWith(child.dataset.filter)) {
+      child.classList.add("active");
+    }
+  }
+
+  for (const child of moduleList.children) {
+    child.classList.add("d-none");
+    child.classList.remove("d-block");
+    child.classList.remove("active");
+    // hash can be namespaced name of the module in that case
+    // filter all modules of that namespace
+    if (hash.startsWith(child.dataset.filter) || hash === "") {
+      child.classList.add("d-block");
+      child.classList.remove("d-none");
+    }
+  }
+
+  if (document.getElementById(hash + "-list")) {
+    document.getElementById(hash + "-list").classList.add("active");
+  }
+
+  for (const child of moduleInfoList.children) {
+    child.classList.add("d-none");
+    child.classList.remove("d-block");
+
+    if (child.id === hash) {
+      child.classList.add("d-block");
+      child.classList.remove("d-none");
+    }
+  }
+}
+
 // Send an XHR request to the server to force a run.
-function forceRun(namespace, module) {
-  console.log(namespace, module)
-  url = window.location.href + "api/v1/forceRun"
+function forceRun(namespace, module, planOnly) {
+  console.log(namespace, module, planOnly)
+  url = window.location.origin + "/api/v1/forceRun"
 
   fetch(url, {
     method: "post",
@@ -25,6 +73,7 @@ function forceRun(namespace, module) {
     body: JSON.stringify({
       namespace: namespace,
       module: module,
+      planOnly: planOnly,
     }),
   })
     .then((response) => response.json())
