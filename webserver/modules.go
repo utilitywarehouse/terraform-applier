98a22b21d3f2948ec41498ee8c48e9f12349b933
Ashok Siyani <ashok.siyani@gmail.com>
2024-02-27 13:34:58 +0000

    remove unused function

--- webserver/modules.go
+++ webserver/modules.go
@@ -5,9 +5,6 @@ import (
 	"sort"
 
 	tfaplv1beta1 "github.com/utilitywarehouse/terraform-applier/api/v1beta1"
-	authorizationv1 "k8s.io/api/authorization/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -44,32 +41,4 @@ func listModules(ctx context.Context, clt client.Client) ([]tfaplv1beta1.Module,
 		return moduleList.Items[i].Namespace+moduleList.Items[i].Name < moduleList.Items[j].Namespace+moduleList.Items[j].Name
 	})
 	return moduleList.Items, nil
-}
-
-// HasAccess returns a boolean depending on whether the email address provided
-// corresponds to a user who has edit access to the specified Module.
-func hasAccess(ctx context.Context, c kubernetes.Interface, module *tfaplv1beta1.Module, email, verb string) (bool, error) {
-	gvk := module.GroupVersionKind()
-	plural := "modules"
-
-	response, err := c.AuthorizationV1().SubjectAccessReviews().Create(
-		ctx,
-		&authorizationv1.SubjectAccessReview{
-			Spec: authorizationv1.SubjectAccessReviewSpec{
-				ResourceAttributes: &authorizationv1.ResourceAttributes{
-					Namespace: module.Namespace,
-					Verb:      verb,
-					Group:     gvk.Group,
-					Version:   gvk.Version,
-					Resource:  plural,
-				},
-				User: email,
-			},
-		},
-		metav1.CreateOptions{},
-	)
-	if err != nil {
-		return false, err
-	}
-	return response.Status.Allowed, nil
 }
