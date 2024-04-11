4a94f92da2e17e1e25aeae446a8dba74af7af511
Ashok Siyani <ashok.siyani@gmail.com>
2024-04-11 08:49:54 +0100

    added missed file

--- api/v1beta1/request.go
+++ api/v1beta1/request.go
@@ -9,13 +9,19 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
+var (
+	ErrRunRequestExist    = fmt.Errorf("another pending run request found")
+	ErrNoRunRequestFound  = fmt.Errorf("no pending run requests found")
+	ErrRunRequestMismatch = fmt.Errorf("run request ID doesn't match pending request id")
+)
+
 // Request represents terraform run request
 type Request struct {
-	types.NamespacedName
-	ID          string       `json:"id,omitempty"`
-	RequestedAt *metav1.Time `json:"reqAt,omitempty"`
-	Type        string       `json:"type,omitempty"`
-	PR          *PullRequest `json:"pr,omitempty"`
+	NamespacedName types.NamespacedName `json:"module,omitempty"`
+	ID             string               `json:"id,omitempty"`
+	RequestedAt    *metav1.Time         `json:"reqAt,omitempty"`
+	Type           string               `json:"type,omitempty"`
+	PR             *PullRequest         `json:"pr,omitempty"`
 }
 
 type PullRequest struct {
@@ -25,10 +31,10 @@ type PullRequest struct {
 }
 
 func (req *Request) Validate() error {
-	if req.Namespace == "" {
+	if req.NamespacedName.Namespace == "" {
 		return fmt.Errorf("namespace is required")
 	}
-	if req.Name == "" {
+	if req.NamespacedName.Name == "" {
 		return fmt.Errorf("name is required")
 	}
 	if req.RequestedAt.IsZero() {
@@ -50,7 +56,7 @@ func (req *Request) Validate() error {
 
 // IsPlanOnly will return is req is plan-only
 func (req *Request) IsPlanOnly(module *Module) bool {
-	// for scheduled and polling run we respect module spec
+	// for scheduled and polling run respect module spec
 	if req.Type == ScheduledRun ||
 		req.Type == PollingRun {
 		return module.IsPlanOnly()
@@ -59,6 +65,12 @@ func (req *Request) IsPlanOnly(module *Module) bool {
 	// this is override triggered by user
 	if req.Type == ForcedApply {
 		return false
+	}
+
+	// these are plan only override requests
+	if req.Type == PRPlan ||
+		req.Type == ForcedPlan {
+		return true
 	}
 
 	// its always safe to default to plan-only
