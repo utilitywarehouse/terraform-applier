270db7edc0f23cdebac81a4bc60bd6241c462a0a
Ashok Siyani <ashok.siyani@gmail.com>
2024-04-29 14:22:23 +0100

    help debug run process

--- integration_test/src/modules/hello-with-providers/hello.tf
+++ integration_test/src/modules/hello-with-providers/hello.tf
@@ -4,3 +4,9 @@ resource "null_resource" "slow_provider" {
     command = "echo progressing...;sleep ${var.sleep};echo done"
   }
 }
+
+resource "null_resource" "exec_provider" {
+  provisioner "local-exec" {
+    command = var.command
+  }
+}
--- integration_test/src/modules/hello-with-providers/variables.tf
+++ integration_test/src/modules/hello-with-providers/variables.tf
@@ -2,3 +2,8 @@ variable "sleep" {
   description = "sleep"
   default     = "1"
 }
+
+variable "command" {
+  description = "command"
+  default     = "sleep 1"
+}
