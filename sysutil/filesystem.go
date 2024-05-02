7e5abdaef69ae44cb3f8eee2e2280332d4e88f17
Ashok Siyani <ashok.siyani@gmail.com>
2024-05-02 15:38:16 +0100

    delete old stale plugin from cache folder

--- sysutil/filesystem.go
+++ sysutil/filesystem.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"sync"
 )
 
@@ -112,5 +113,76 @@ func CopyDir(src string, dst string, withReplace bool) error {
 		}
 	}
 
+	return nil
+}
+
+func IsDirEmpty(path string) (bool, error) {
+	dirents, err := os.ReadDir(path)
+	if err != nil {
+		return false, err
+	}
+	return len(dirents) == 0, nil
+}
+
+func RemoveDirContentsRecursiveIf(dir string, fn func(path string, fi os.FileInfo) (bool, error)) error {
+	var errs []error
+
+	// check if any file/dir needs to be removed from current dir
+	if err := RemoveDirContentsIf(dir, fn); err != nil {
+		errs = append(errs, err)
+	}
+
+	// read current dir and check sub directories
+	dirEnts, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	for _, fi := range dirEnts {
+		if !fi.IsDir() {
+			continue
+		}
+		p := filepath.Join(dir, fi.Name())
+		if err := RemoveDirContentsRecursiveIf(p, fn); err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) != 0 {
+		return fmt.Errorf("%s", errs)
+	}
+
+	return nil
+}
+
+// RemoveDirContentsIf iterated the specified dir and removes entries
+// if given function returns true for the given entry
+func RemoveDirContentsIf(dir string, fn func(path string, fi os.FileInfo) (bool, error)) error {
+	dirEnts, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	// Save errors until the end.
+	var errs []error
+	for _, fi := range dirEnts {
+		p := filepath.Join(dir, fi.Name())
+		stat, err := os.Stat(p)
+		if err != nil {
+			return err
+		}
+		if shouldDelete, err := fn(p, stat); err != nil {
+			return err
+		} else if !shouldDelete {
+			continue
+		}
+		if err := RemoveAll(p); err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) != 0 {
+		return fmt.Errorf("%s", errs)
+	}
 	return nil
 }
